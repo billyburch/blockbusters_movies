d9665045cfa0e10d91e27f3f7e88619b17e79268
Billy Burch <billyburch40@gmail.com>
2023-10-16 17:30:31 -0600

    feat: add feature test files

--- README.md
+++ README.md
@@ -1,5 +1,89 @@
 # README
 
+Iteration 1
+CRUD
+
+[x] done
+User Story 1, blockbuster Index 
+
+For each blockbuster table
+As a visitor
+When I visit '/blockbusters'
+Then I see the name of each blockbuster record in the system
+
+[x] done
+User Story 2, blockbuster Show 
+
+As a visitor
+When I visit '/blockbusters/:id'
+Then I see the blockbuster with that id including the blockbuster's attributes
+(data from each column that is on the blockbuster table)
+
+[x] done
+User Story 3, movie Index 
+
+As a visitor
+When I visit '/movie_table_name'
+Then I see each movie in the system including the movie's attributes
+(data from each column that is on the movie table)
+
+[ ] done
+User Story 4, movie Show 
+
+As a visitor
+When I visit '/movie_table_name/:id'
+Then I see the movie with that id including the movie's attributes
+(data from each column that is on the movie table)
+
+[ ] done
+User Story 5, blockbuster movieren Index 
+
+As a visitor
+When I visit '/blockbusters/:blockbuster_id/movie_table_name'
+Then I see each movie that is associated with that blockbuster with each movie's attributes
+(data from each column that is on the movie table)
+
+ActiveRecord
+
+[ ] done
+User Story 6, blockbuster Index sorted by Most Recently Created 
+
+As a visitor
+When I visit the blockbuster index,
+I see that records are ordered by most recently created first
+And next to each of the records I see when it was created
+
+[ ] done
+User Story 7, blockbuster movie Count
+
+As a visitor
+When I visit a blockbuster's show page
+I see a count of the number of movieren associated with this blockbuster
+Usability
+
+[ ] done
+User Story 8, movie Index Link
+
+As a visitor
+When I visit any page on the site
+Then I see a link at the top of the page that takes me to the movie Index
+
+[ ] done
+User Story 9, blockbuster Index Link
+
+As a visitor
+When I visit any page on the site
+Then I see a link at the top of the page that takes me to the blockbuster Index
+
+[ ] done
+User Story 10, blockbuster movie Index Link
+
+As a visitor
+When I visit a blockbuster show page ('/blockbusters/:id')
+Then I see a link to take me to that blockbuster's `movie_table_name` page ('/blockbusters/:id/movie_table_name')
+
+<!-- 
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
@@ -21,4 +105,4 @@ Things you may want to cover:
 
 * Deployment instructions
 
-* ...
+* ... -->
---
+++ spec/features/blockbusters_movies_spec.rb
@@ -0,0 +1,96 @@
+require "rails_helper"
+
+RSpec.describe "Blockbusters Index", type: :feature do
+  before(:each) do
+    @blockbuster1 = Blockbuster.create!(name:"Huntsman Square", city:"Springfield", state:"Virginia", store_awards:12, rewards_program: true)
+    @blockbuster2 = Blockbuster.create!(name:"Colfax", city:"Denver", state:"Colorado", store_awards:2, rewards_program: false)
+    @blockbuster3 = Blockbuster.create!(name:"Folly Beach", city:"Charleston", state:"South Carolina", store_awards: 5, rewards_program: true)
+    @movie1 = @blockbuster1.movies.create!(title:"It", description:"Spooky Clown", genre:"Horror", release_year: 2017, restricted: true)
+    @movie2 = @blockbuster1.movies.create!(title:"Elf", description:"Charming Odyssey", genre:"Holiday Comedy", release_year: 2003, restricted: false)
+  end
+
+  describe "user story 1, Blockbuster Index" do
+    describe "For each blockbuster table" do
+      describe "as a visitor" do
+        describe "when I visit /blockbusters" do
+          it "then I see the name of each blockbuster record in the system" do
+            #Arrange/Given
+              #before(:each)
+            #Act/When
+            visit "/blockbusters"
+            #Assert/Then
+            expect(page).to have_content(@blockbuster1.name)
+            expect(page).to have_content(@blockbuster2.name)
+            expect(page).to have_content(@blockbuster3.name)
+          end
+        end
+      end
+    end
+  end
+
+  describe "User story 2, Blockbuster Show" do
+    describe "As a visitor" do
+      describe "When I visit '/blockbusters/:id'
+      " do
+        it "Then I see the blockbuster with that id including the blockbuster's attributes
+        " do
+          #Arrange/Given
+            #before(:each)
+          #Act/When
+          visit "/blockbusters/#{@blockbuster1.id}"
+          #Assert/Then
+          expect(page).to have_content(@blockbuster1.name)
+          expect(page).to have_content(@blockbuster1.city)
+          expect(page).to have_content(@blockbuster1.state)
+          expect(page).to have_content(@blockbuster1.store_awards)
+          expect(page).to have_content(@blockbuster1.rewards_program)
+        end
+      end
+    end
+  end
+
+  describe "User Story 3, Movie Index " do
+    describe "As a visitor" do
+      describe "When I visit '/movies'" do
+        it "Then I see each Movie in the system including the Movie's attributes" do
+          #Arrange/Given
+            #before(:each)
+          #Act/When
+          visit "/movies"
+          #Assert/Then
+          expect(page).to have_content(@movie1.title)
+          expect(page).to have_content(@movie1.description)
+          # expect(page).to have_content(@movie1.genre)
+          # expect(page).to have_content(@movie1.release_year)
+          # expect(page).to have_content(@movie1.restricted)
+
+          expect(page).to have_content(@movie2.title)
+          expect(page).to have_content(@movie2.description)
+          # expect(page).to have_content(@movie2.genre)
+          # expect(page).to have_content(@movie2.release_year)
+          # expect(page).to have_content(@movie2.restricted)
+        end
+      end
+    end
+  end
+
+  describe "User Story 4, movie Show" do
+    describe "As a visitor" do
+      describe "When I visit '/movies/:id" do
+        it "Then I see the movie with that id including the movie's attributes" do
+          #Arrange/Given
+            #before(:each)
+          #Act/When
+          visit "/movies/#{@movie1.id}"
+          #Assert/Then
+          expect(page).to have_content(@movie1.title)
+          expect(page).to have_content(@movie1.description)
+          expect(page).to have_content(@movie1.genre)
+          expect(page).to have_content(@movie1.release_year)
+          expect(page).to have_content(@movie1.restricted)        
+        end
+      end
+    end
+  end
+ 
+end
